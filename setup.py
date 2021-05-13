d01d61b9d9153227010221ec9b343a9e8d735fae
lorenzo-park <career.lorenzo.park@gmail.com>
2021-05-13 17:05:55 +0900

    fix

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup
-from pip.req import parse_requirements
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
 
 setup(name='docparser',
       version='0.1',
